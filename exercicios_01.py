8a1af8675f04bf25c0c71878cd638ad9a7ec4194
Yarles de Andrade Espirito Santo <yarles100@gmail.com>
2024-08-27 22:15:05 -0300

    feat: iniciado implementação de exercicios zoologico

---
+++ exercicios_01.py
@@ -0,0 +1,37 @@
+# filtrar todos  animais femea
+
+def filtrar_animais_femea(animais):
+    pass
+
+
+def filtrar_animais_machos_acima_de_4_anos_de_idade(animais):
+    pass
+
+def filtrar_animais_femeas_abaixo_ou_igual_a_4_anos_de_idade(animais):
+    pass
+
+# função com maior complexidade
+# filtrar todos os animais em pacotes de informações por genero
+# exemplo: {'macho': [animal1, animal2], 'femea': [animal3, animal4]}
+
+def filtrar_animais_por_genero(animais):
+    pass
+
+# função com maior complexidade
+# filtrar todos os animais em pacotes de informações por genero e idade
+""""
+exemplo:
+{
+    'macho': {
+        'acima_de_4_anos': [animal1, animal2],
+        'abaixo_de_4_anos': [animal3, animal4]
+    },
+    'femea': {
+        'acima_de_4_anos': [animal5, animal6],
+        'abaixo_de_4_anos': [animal7, animal8]
+    }
+}
+"""
+
+def filtrar_animais_por_genero_e_idade(animais):
+    pass
